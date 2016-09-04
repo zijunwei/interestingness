4dbee2cc36887f49adbcd217a8b06e2047da49a2
Zijun Wei <zijwei@cs.stonybrook.edu>
2016-09-04 00:29:15 -0400

    update in Mac version Transfering screen coordinates into video coordiantes has been done in file: preprocessing_convert_coordinates

--- gaze_in_videos/hollywood2/convert2video_coord.m
+++ gaze_in_videos/hollywood2/convert2video_coord.m
@@ -1,4 +1,4 @@
-function loc_video=convert2video_coord (loc_screen.video_res)
+function loc_video=convert2video_coord (loc_screen, video_res)
 % according to readme.txt, convert location in screen to location in videos
 % video_res [x,y];
 % loc_screen [x,y];
@@ -10,17 +10,17 @@ function loc_video=convert2video_coord (loc_screen.video_res)
 
 % screen_res are from geomtry.txt
 
-screen_res_x=1280;
-screen_res_y=1024;
+screen_res_x=1280.0;
+screen_res_y=1024.0;
 
 
-video_res_x=video_res(1);
-video_res_y=video_res(2);
+video_res_x=double( video_res(1));
+video_res_y=double( video_res(2));
 
 
 
-a=video_res_x/screen_res_x;
-b=(screen_res_y-video_res_y/a)/2;
+a= video_res_x/ screen_res_x;
+b=  ( screen_res_y-video_res_y/a)/2;
 loc_video=[a*loc_screen(:,1),a*(loc_screen(:,2)-b)];
 
 end
